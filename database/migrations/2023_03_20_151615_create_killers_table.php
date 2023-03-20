393260e671c1fbe69ab6fb4a01f9bd086151afae
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-20 16:30:20 +0100

    add model and migration

---
+++ database/migrations/2023_03_20_151615_create_killers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('killers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('age')->nullable();
+            $table->integer('kills')->nullable();
+            $table->boolean('wanted');
+            $table->url('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('killers');
+    }
+};
