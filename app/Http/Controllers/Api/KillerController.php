1887540fb15bbda2804154180754a20ffb984266
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-21 15:36:04 +0100

    add index in KillerController(API)

---
+++ app/Http/Controllers/Api/KillerController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Killer;
+use Illuminate\Http\Request;
+
+class KillerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $killers = Killer::orderBy('updated_at', 'DESC')->get();
+
+        foreach ($killers as $killer) {
+            if ($killer->picture) $killer->picture = url('storage/' . $killer->picture);
+            else $killer->image = 'https://marcolanci.it/utils/placeholder.jpg';
+        }
+
+        return response()->json($killers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
